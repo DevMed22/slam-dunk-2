5b13e88858499fd350252e76dd4e4829fe88c447
Amr Atif <62966382+amr-atif@users.noreply.github.com>
2022-09-30 00:46:16 +0200

    create accounts app
    users now can login and signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+class SignUpView(CreateView):
+    success_url = reverse_lazy ('login')
+    template_name = 'registration/signup.html'
--- root/urls.py
+++ root/urls.py
@@ -22,11 +22,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    #path('', include('pages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls'))
 ]
 
 
-urlpatterns += staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
