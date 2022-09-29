5a344e3b82a3447fd4f6dfc4893661f1b1ce736d
Anas Osama <anasosama914@gmail.com>
2022-09-30 00:13:45 +0200

    Update Urls
    add urls to staticfiles and media

--- root/urls.py
+++ root/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +26,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls'))
 ]
+
+
+urlpatterns += staticfiles_urlpatterns
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
