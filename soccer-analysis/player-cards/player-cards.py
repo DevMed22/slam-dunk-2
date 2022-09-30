8064164d0c3aabf068b3adab5c21ee879cf0ef57
ahmed tarek elsayed <77227944+ahmedtarek26@users.noreply.github.com>
2022-10-01 01:46:29 +0200

    player-cards

---
+++ soccer-analysis/player-cards/player-cards.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  1 01:23:21 2022
+
+@author: Ahmed Tarek """
+
+import pandas as pd
+'''  male player features needed :
+    short_name , overall, club_position,age , club_name ,  pace , 
+shooting ,passing , dribbling , defending ,physic
+'''
+m_players = pd.read_csv('players_22.csv')
+
+'''  male player features needed :
+    short_name , overall, player_positions , age ,  pace , 
+shooting ,passing , dribbling , defending ,physic
+'''
+
+f_players = pd.read_csv('female_players_22.csv')
