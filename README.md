6f3112f0a6cb7138a2f598b3578c85cf3becf24a
Amr Atif <62966382+amr-atif@users.noreply.github.com>
2022-10-02 05:14:30 +0200

    front end bugs resolved

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # slam-dunk-2
 A professional website for visualising data for football competitions and football data with an easy way for users to use
+
+## Inspiration
+Many people have a trouble with analyzing the soccer matches, and this give us ideas to implement it and share with other football fans, So they can see stats of matches with details.
+be updated with players stats, also for female players we added stats for them so any player F/M can see stats.
+and share a game with friends to create funny cards for them or any friends.
+## What it does
+The purpose of the app is to make people understand what happend in the match with more details and analysis that will visualize to them how manay shots , drribles, pass maps, and save you from missing any match.
+## How we built it
+We used HTML, CSS, JavaScript and Bootstrap for Frontend. We used Python/Django to build the Backend. We used pandas, numpy, matplotlib, statsbombpy for soccer open data, and mplsoccer for pitch plot and visualization.We used streamlit to build the frontend of the Analysis apps.
+## Challenges we ran into
+We had some errors and made our search to solve them. The time was challenging although we began our project just 24 hours ago. We face some problems dealing with GitHub repo to distribute work between us but with try and error we got our way. We faced some problems in connecting the frontend to the backend as we are developing our Django app in a short time.
+## Accomplishments that we're proud of
+We have built this project in less than 24 hour from zero to hero. Our Analysis apps integrates well with the website and ready to be used by users. We collaborated together in a professional way through using GitHub and Discord. The pair programming was really great skill to practice and we enjoyed the process and proud of our progress.
+## What we learned
+We have sharpened our teamwork skills through distributing workload and managing our work using GitHub project boards. Everyone has sharpened his technology and understanding of it within the context of a complete project through integration of different technologies. Brainstorming our ideas together and respecting others opinions and suggestions so finally we vote for our best solution.
+## What's next for Soccer Viz
+Adding more details about players such as career info, goals in career, clubs and national team stats.
+Adding more featurs in the competitions, analysis for each player in the match. Passing network plots.
--- soccer-analysis/player-cards/player-cards.py
+++ soccer-analysis/player-cards/player-cards.py
@@ -1,19 +1,53 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 """
 Created on Sat Oct  1 01:23:21 2022
 
 @author: Ahmed Tarek """
-
+import streamlit as st
 import pandas as pd
-'''  male player features needed :
-    short_name , overall, club_position,age , club_name ,  pace , 
-shooting ,passing , dribbling , defending ,physic
-'''
-m_players = pd.read_csv('players_22.csv')
 
-'''  male player features needed :
-    short_name , overall, player_positions , age ,  pace , 
-shooting ,passing , dribbling , defending ,physic
-'''
+
+m_players = pd.read_csv('players_22.csv')
+player_id = []
+player_name = []
+for i in range(len(m_players)):
+    player_name.append(m_players['short_name'][i])
+    player_id.append(i)
+m_players_dict = dict(zip(player_name, player_id))
 
 f_players = pd.read_csv('female_players_22.csv')
+f_player_id = []
+f_player_name = []
+for i in range(len(f_players)):
+    f_player_name.append(f_players['short_name'][i])
+    f_player_id.append(i)
+f_players_dict = dict(zip(f_player_name, f_player_id))
+
+## App
+st.title('Discover stats of your favourite player 🏃‍♂️🤸‍♀️')
+gender = st.selectbox('Player gender', ('Male', 'Female'))
+if gender == 'Male':
+    player = st.selectbox('Player name', player_name)
+    st.write(f'Player name : {player}')
+    st.write(f"overall rank : {m_players['overall'][m_players_dict[player]]}")
+    st.write(f"Position : {m_players['club_position'][m_players_dict[player]]}")
+    st.write(f"Age : {m_players['age'][m_players_dict[player]]}")
+    st.write(f"Club : {m_players['club_name'][m_players_dict[player]]}")
+    st.write(f"PAC: {m_players['pace'][m_players_dict[player]]}")
+    st.write(f"Shooting : {m_players['shooting'][m_players_dict[player]]}")
+    st.write(f"Passing : {m_players['passing'][m_players_dict[player]]}")
+    st.write(f"Dribbling: {m_players['dribbling'][m_players_dict[player]]}")
+    st.write(f"Defending : {m_players['defending'][m_players_dict[player]]}")
+    st.write(f"Physic: {m_players['physic'][m_players_dict[player]]}")
+elif gender == 'Female':
+    player = st.selectbox('Female Player name', f_player_name)
+    st.write(f'Player name : {player}')
+    st.write(f"overall rank : {f_players['overall'][f_players_dict[player]]}")
+    st.write(f"Position : {f_players['player_positions'][f_players_dict[player]]}")
+    st.write(f"Age : {f_players['age'][f_players_dict[player]]}")
+    st.write(f"PAC: {f_players['pace'][f_players_dict[player]]}")
+    st.write(f"Shooting : {f_players['shooting'][f_players_dict[player]]}")
+    st.write(f"Passing : {f_players['passing'][f_players_dict[player]]}")
+    st.write(f"Dribbling: {f_players['dribbling'][f_players_dict[player]]}")
+    st.write(f"Defending : {f_players['defending'][f_players_dict[player]]}")
+    st.write(f"Physic: {f_players['physic'][f_players_dict[player]]}")
